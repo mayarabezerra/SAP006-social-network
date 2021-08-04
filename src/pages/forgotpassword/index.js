5da8c337985f31843b3b89db08fa364839e58934
mayarabezerra <mayarab253@gmail.com>
2021-08-04 19:28:59 -0300

    função reset senha funcionando, com página e rotas funcionando

---
+++ src/pages/forgotpassword/index.js
@@ -0,0 +1,48 @@
+import {reset} from '../../services/index.js'
+
+export const forgot = () => {
+    const rootForgot = document.createElement('div');
+    const contentForgot = `
+    <section class="section-exemple-three">
+    <div class="shadow"></div>
+    <div class="container-forgot">
+        <div class="content-forgot">
+            <section id="forgot-container" class="forgot-container"> 
+                <h1>Redefinir senha</h1>
+    
+                <form action="">
+                    <label for="email" id="labelEmail">E-mail</label>
+                    <input type="email" name="email" id="email" placeholder="nome@email.com" autocomplete="off">
+</form>
+        <button class="reset" id="reset-btn">Enviar</button>
+        </section>
+            </div>
+            `;
+
+rootForgot.innerHTML = contentForgot;
+const btnReset = rootForgot.querySelector('#reset-btn')
+
+btnReset.addEventListener('click', () => {
+    const email = document.getElementById('email').value;
+        reset(email);
+      });
+
+
+return rootForgot
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/*btnReset.addEventListener('click', () => {
+    const email = document.getElementById('email').value;
+        resetPassword(email);
+      });*/
