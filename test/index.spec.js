72fe6c438b1c30119316372e434fb28e2b009300
tenorionique <monique_doretto@hotmail.com>
2021-09-01 19:51:18 -0300

    testes em arquivos separados

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,4 @@
-/**
- * @jest-environment jsdom
- */
-import { sendVerificationEmail, loginOfUser } from '../src/services/index.js';
-import { loginUsuario } from '../src/pages/login/index.js';
+import { sendVerificationEmail } from '../src/services/index.js';
 /* import { getFirebase } from '../src/services/firebase.js'; */
 
 const mockSendEmail = jest.fn();
@@ -22,35 +18,10 @@ jest.mock('../src/services/firebase.js', () => ({
   })),
 }));
 
-jest.mock('../src/services/index.js', () => ({
-  loginOfUser: jest.fn(() => Promise.resolve({
-    user: 'teste',
-  })),
-}));
-
 describe('sendVerificationEmail', () => {
   it('should be a function', () => {
     sendVerificationEmail();
     expect(mockSendEmail).toHaveBeenCalledTimes(1);
-  });
-});
-
-describe('loginPage', () => {
-  it('should return login page', () => {
-    const page = loginUsuario();
-    page.querySelector('#email').value = 'exemplo@exemplo.com';
-    /* page.querySelector('.password').value = '12345678'; */
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '1' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '2' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '3' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '4' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '5' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '6' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '7' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '8' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '9' }));
-    page.querySelector('#submit-btn').click();
-    expect(loginOfUser).toHaveBeenCalledTimes(1);
   });
 });
 
