2aa0f6cf080a490cfe56605356d27d1020bc8ed8
tenorionique <monique_doretto@hotmail.com>
2021-09-02 13:03:45 -0300

    teste logOut

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,8 @@
-import { sendVerificationEmail } from '../src/services/index.js';
+import { sendVerificationEmail, logOut } from '../src/services/index.js';
 /* import { getFirebase } from '../src/services/firebase.js'; */
 
 const mockSendEmail = jest.fn();
+const mockLogOut = jest.fn();
 jest.mock('../src/services/firebase.js', () => ({
   getFirebase: jest.fn(() => ({
     firestore: jest.fn(() => ({
@@ -14,6 +15,9 @@ jest.mock('../src/services/firebase.js', () => ({
       signInWithEmailAndPassword: Promise.resolve({
         user: 'teste',
       }),
+      signOut: mockLogOut
+        .mockResolvedValueOnce('first call')
+        .mockRejectedValueOnce(new Error('Async error')),
     })),
   })),
 }));
@@ -22,6 +26,14 @@ describe('sendVerificationEmail', () => {
   it('should be a function', () => {
     sendVerificationEmail();
     expect(mockSendEmail).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('logOut', () => {
+  console.log(mockLogOut);
+  it('should be called once', () => {
+    logOut();
+    expect(mockLogOut).toHaveBeenCalledTimes(1);
   });
 });
 
--- test/login.spec.js
+++ test/login.spec.js
@@ -4,7 +4,45 @@
 import { loginUsuario } from '../src/pages/login/index.js';
 import { loginOfUser } from '../src/services/index.js';
 
-jest.mock('../src/services/index.js', () => ({
+const mockLogin = jest.fn();
+jest.mock('../src/services/firebase.js', () => ({
+  getFirebase: jest.fn(() => ({
+    firestore: jest.fn(() => ({
+      collection: jest.fn(),
+    })),
+    auth: jest.fn(() => ({
+      signInWithEmailAndPassword: mockLogin,
+    })),
+  })),
+}));
+
+/* jest.mock('../src/services/index.js', () => ({
+  loginOfUser: jest.fn(() => Promise.resolve({
+    user: 'teste',
+  })),
+})); */
+
+describe('loginPage', () => {
+  it('should return login page', () => {
+    const page = loginUsuario();
+    page.querySelector('#email').value = 'exemplo@exemplo.com';
+    /* page.querySelector('.password').value = '12345678'; */
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '1' }));
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '2' }));
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '3' }));
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '4' }));
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '5' }));
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '6' }));
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '7' }));
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '8' }));
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '9' }));
+    page.querySelector('#submit-btn').click();
+    loginOfUser();
+    expect(mockLogin).toHaveBeenCalledTimes(1);
+  });
+});
+
+/* jest.mock('../src/services/index.js', () => ({
   loginOfUser: jest.fn(() => Promise.resolve({
     user: 'teste',
   })),
@@ -14,16 +52,16 @@ describe('loginPage', () => {
     const page = loginUsuario();
     page.querySelector('#email').value = 'exemplo@exemplo.com';
     page.querySelector('.password').value = '12345678';
-    /*  page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '1' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '2' }));
+    /*  page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '1' })); */
+/* page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '2' }));
     page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '3' }));
     page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '4' }));
     page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '5' }));
     page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '6' }));
     page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '7' }));
     page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '8' }));
-    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '9' })); */
+    page.querySelector('.password').dispatchEvent(new KeyboardEvent('keyup', { key: '9' }));
     page.querySelector('#submit-btn').click();
     expect(loginOfUser).toHaveBeenCalledTimes(1);
   });
-});
+}); */
